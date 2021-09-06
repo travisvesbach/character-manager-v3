7ad3e6e251cbd2a9bf03f939569dd054d89b5708
Travis Vesbach <tav1990@hotmail.com>
2021-09-06 09:32:06 -0500

    Add user tests

---
+++ tests/Feature/UsersTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class UsersTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test **/
+    public function first_user_is_an_admin() {
+        $user = User::factory()->create();
+        $this->assertTrue($user->admin);
+    }
+
+    /** @test **/
+    public function users_added_after_the_first_are_not_admins() {
+        $this->assertTrue(User::factory()->create()->admin);
+        $this->assertNull(User::factory()->create()->admin);
+        $this->assertNull(User::factory()->create()->admin);
+    }
+
+    /** @test **/
+    public function guests_cannot_manage_users() {
+        $this->get('/users')->assertRedirect('login');
+        $this->get('/users/create')->assertRedirect('login');
+        $this->post('users', $attributes = [
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'admin' => false,
+            ])
+            ->assertRedirect('login');
+        $this->assertCount(0, User::all());
+
+        $other_user = User::factory()->create();
+        $this->patch('users/' . $other_user->id, $attributes)->assertRedirect('login');
+        $this->delete('users/' . $other_user->id)->assertRedirect('login');
+        $this->assertCount(1, User::all());
+    }
+
+    /** @test **/
+    public function non_admins_cannot_manage_users() {
+        $admin_user = User::factory()->create();
+        $this->signIn();
+
+        $this->get('/users')->assertStatus(403);
+        $this->get('/users/create')->assertStatus(403);
+        $this->post('users', $attributes = [
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'admin' => false,
+            ])
+            ->assertStatus(403);
+        $this->assertCount(2, User::all());
+
+        $other_user = User::factory()->create();
+        $this->patch('users/' . $other_user->id, $attributes)->assertStatus(403);
+        $this->delete('users/' . $other_user->id)->assertStatus(403);
+        $this->assertCount(3, User::all());
+    }
+
+    /** @test **/
+    public function an_admin_can_create_users() {
+        $admin_user = User::factory()->create();
+
+        $this->actingAs($admin_user)
+            ->post('users', [
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'admin' => false,
+            ])
+            ->assertRedirect('users');
+
+        $this->assertCount(2, User::all());
+    }
+
+    /** @test **/
+    public function an_admin_can_update_users() {
+        $admin_user = User::factory()->create();
+
+        $user_to_update = User::factory()->create();
+
+        $this->actingAs($admin_user)
+            ->patch('users/' . $user_to_update->id, [
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'admin' => false,
+            ])
+            ->assertRedirect('users');
+
+        $this->assertCount(2, User::all());
+    }
+
+    /** @test **/
+    public function an_admin_can_delete_users() {
+        $admin_user = User::factory()->create();
+        $delete_user = User::factory()->create();
+        $this->assertCount(2, User::all());
+
+        $this->actingAs($admin_user)
+            ->delete('users/' . $delete_user->id)
+            ->assertRedirect('users');
+
+        $this->assertCount(1, User::all());
+    }
+}
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use Facades\Tests\Setup\ProjectFactory;
+
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    // /** @test **/
+    // public function a_user_has_projects() {
+    //     $user = User::factory()->create();
+
+    //     $this->assertInstanceOf(Collection::class, $user->projects);
+    // }
+
+    // /** @test **/
+    // public function a_user_has_all_projects() {
+    //     $john = $this->signIn();
+
+    //     ProjectFactory::ownedBy($john)->create();
+
+    //     $this->assertCount(1, $john->allProjects());
+
+    //     $jane = User::factory()->create();
+    //     $bill = User::factory()->create();
+
+    //     $project = tap(ProjectFactory::ownedBy($jane)->create())->invite($bill);
+
+    //     $this->assertCount(1, $john->allProjects());
+
+    //     $project->invite($john);
+
+    //     $this->assertCount(2, $john->allProjects());
+    // }
+}
