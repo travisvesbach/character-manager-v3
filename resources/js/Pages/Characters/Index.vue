290e7713410dd7f3af4f0cb8eae26f167fd6338a
Travis Vesbach <tav1990@hotmail.com>
2021-10-25 18:45:42 -0500

    Fix typo

--- resources/js/Pages/Characters/Index.vue
+++ resources/js/Pages/Characters/Index.vue
@@ -30,7 +30,7 @@
                             Characters are Player Characters.  Start by creating a character.  When viewing a character, a seconday navigation bar is visibile that can be used to quickly switch between active (non-archived) characters.
                         </p>
                         <p class="mb-4">
-                            Characters can have actions, resources, spells, modifiers, etc. once they are created.  All stats can be rolled by click in them.  Using the dropdown next the a character's name, different sections of the character sheet can be shown/hidden as necessary.
+                            Characters can have actions, resources, spells, modifiers, etc. once they are created.  All stats can be rolled by click on them.  Using the dropdown next the a character's name, different sections of the character sheet can be shown/hidden as necessary.
                         </p>
                         <p class="mb-4">
                             Archiving a character removes it from the main index list as well as from the secondary navigation bar.  Archiving is useful if you have characters that don't get used often or anymore and you don't want them taking up space in the secondary navbar.
